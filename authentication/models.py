e1a9f17befbd870a621a8aac39f3616337ebafe0
Orphée <letembeorphee@gmail.com>
2022-03-24 08:18:59 +0100

    update str method

--- authentication/models.py
+++ authentication/models.py
@@ -8,5 +8,5 @@ class User(AbstractUser):
         verbose_name_plural = 'Utilisateurs'
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.username
 
