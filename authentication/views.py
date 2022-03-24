a919661c219d568845c7e51a3529f1b6b549d557
Orphée <letembeorphee@gmail.com>
2022-03-24 08:17:11 +0100

    Add response http

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -7,7 +6,6 @@ from .serializers import UserSerializer
 
 
 class SignupView(APIView):
-    permission_classes = (AllowAny,)
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
