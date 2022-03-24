27fdbddec36daa5515e00beb9426cb2a53c61b75
Orphée <letembeorphee@gmail.com>
2022-03-24 08:17:48 +0100

    update perm user

--- project/permissions.py
+++ project/permissions.py
@@ -3,10 +3,6 @@ from rest_framework import permissions
 
 class IsAuthor(permissions.BasePermission):
 
-    def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
-
     def has_object_permission(self, request, view, obj):
 
         if request.method in permissions.SAFE_METHODS:
