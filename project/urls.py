dbb5d9433fcda822a824f11018e05c3274152ebc
Orphée <letembeorphee@gmail.com>
2022-03-24 08:20:48 +0100

    update urls

--- project/urls.py
+++ project/urls.py
@@ -1,22 +1,18 @@
 from django.urls import path
 
-from .views import ProjectListView, ProjectDetailView, GetAddUserView, DeleteUserView, GetAddIssueView, \
-    PutDeleteIssueView, GetAddCommentView, DeleteCommentView
+from .views import ProjectListView, ProjectDetailView, GetAddContributorView, DeleteContributorView, GetAddIssueView, \
+    PutDeleteIssueView, GetAddCommentView, PutDeleteCommentView
 
 urlpatterns = [
 
     path('projects/', ProjectListView.as_view()),
     path('projects/', ProjectDetailView.as_view()),
     path('projects/<int:id>/', ProjectDetailView.as_view()),
-    path('projects/<int:id>/users/', GetAddUserView.as_view()),
-    path('projects/<int:project_id>/users/<int:user_id>', DeleteUserView.as_view()),
+    path('projects/<int:id>/users/', GetAddContributorView.as_view()),
+    path('projects/<int:project_id>/users/<int:user_id>', DeleteContributorView.as_view()),
     path('projects/<int:id>/issues/', GetAddIssueView.as_view()),
     path('projects/<int:project_id>/issues/<int:issue_id>', PutDeleteIssueView.as_view()),
     path('projects/<int:project_id>/issues/<int:issue_id>/comments/', GetAddCommentView.as_view()),
-    path('projects/<int:project_id>/issues/<int:issue_id>/comments/<int:comment_id>', DeleteCommentView.as_view()),
+    path('projects/<int:project_id>/issues/<int:issue_id>/comments/<int:comment_id>', PutDeleteCommentView.as_view()),
 
     ]
-
-
-
-
